6827efbb19d94f3d841be3a552a5a97b42c38108
anupkumar-mca-hbtu <123875466+anupkumar-mca-hbtu@users.noreply.github.com>
2026-07-16 00:27:45 +0530

    Time: 4 ms (38.44%), Space: 43.4 MB (26.4%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-
+        
         Stack<Character> st= new Stack<>();
 
         for(int i=0;i<s.length();i++) {
@@ -16,5 +16,6 @@ class Solution {
            }
         }
         return st.isEmpty();
+   
     }
 }
